bed7b4f4e89c0ea3072303317e94d9ced08d8eab
VimalnathAchuthan <va33@illinois.edu>
2024-11-18 13:03:22 -0600

    Implemented optimizer

--- src/graph.py
+++ src/graph.py
@@ -4,21 +4,41 @@
     FilePath: src/graph.py
     Description:
 '''
-
+import pandas as pd
 
 class Graph(object):
     def __init__(self, name: str, file_path: str):
         self._name = name
         self._path = file_path
-        # TODO: begin of your codes
-        '''
-        you can add new fields and methods to this class
-        please correctly set the following fields in this method
-        '''
         self._is_undirected = True
         self._vertex_count = -1
         self._edge_count = -1
-        # TODO: end of your codes
+        self.df = None
+
+        if not file_path:
+            self._vertex_count = 0
+            self._edge_count = 0
+            self._is_undirected = True
+            self.df = pd.DataFrame(columns=['src_id', 'dst_id', 'src_label', 'dst_label', 'edge_label'])
+            return
+
+        df = pd.read_csv(file_path, sep=' ', header=None,
+                         names=['src_id', 'dst_id', 'src_label', 'dst_label', 'edge_label'])
+
+        self.df = df
+
+        vertices = pd.concat([df['src_id'], df['dst_id']]).unique()
+        self._vertex_count = len(vertices)
+
+        df['min_id'] = df[['src_id', 'dst_id']].min(axis=1)
+        df['max_id'] = df[['src_id', 'dst_id']].max(axis=1)
+        df['edge_key'] = df['min_id'].astype(str) + '_' + df['max_id'].astype(str) + '_' + df['edge_label'].astype(str)
+
+        edge_counts = df.groupby('edge_key').size()
+
+        self._edge_count = len(edge_counts)
+
+        self._is_undirected = all(edge_counts == 2)
 
     def print_statistics(self) -> None:
         density = self._edge_count / self._vertex_count
--- src/optimizer.py
+++ src/optimizer.py
@@ -8,17 +8,17 @@
 
 from src.graph import Graph
 from src.report import get_file, report
+import pandas as pd
+import numpy as np
+import itertools
+import os
 
 
 class Optimizer(object):
     def __init__(self, data: str, pattern: str):
         self._data_file = data
         self._pattern_file = pattern
-        self._plan = [
-            ['u1,u2', 100],
-            ['u1,u3', 300],
-            ['u3,u4', 20],
-        ]
+        self._plan = []
         # TODO: begin of your codes
         '''
         self._plan explanation:
@@ -38,16 +38,103 @@ class Optimizer(object):
     def run(self):
         self._data = Graph('data', self._data_file)
         self._pattern = Graph('pattern', self._pattern_file)
-        self._sample = Graph('sample', '')
-        # TODO: begin of your codes
-        '''
-        requirement:
-            1. the plan can be represented in any ways
-            2. you can add new fields and methods, and use them here
-            3. this method can return anything, no requirements
-            4. please set self._plan correctly
-        '''
-        # TODO: end of your codes
+
+        sample_fraction = 0.1
+        df_sampled = self._data.df.sample(frac=sample_fraction, random_state=42)
+        df_sampled = df_sampled.reset_index(drop=True)
+
+        sample_file_path = os.path.join('out', 'sample_graph.txt')
+        os.makedirs('out', exist_ok=True)
+        df_sampled.to_csv(sample_file_path, sep=' ', index=False, header=False)
+
+        self._sample = Graph('sample', sample_file_path)
+
+        df_pattern = self._pattern.df
+        pattern_vertices = pd.unique(df_pattern[['src_id', 'dst_id']].values.ravel('K'))
+        pattern_vertices.sort()
+        variable_names = ['u{}'.format(i+1) for i in range(len(pattern_vertices))]
+        dict_pv = dict(zip(pattern_vertices, variable_names))
+        dict_pv_rev = {v: k for k, v in dict_pv.items()}
+
+        relations = []
+
+        df_P = df_pattern.copy()
+        df_P['min_id'] = df_P[['src_id', 'dst_id']].min(axis=1)
+        df_P['max_id'] = df_P[['src_id', 'dst_id']].max(axis=1)
+        df_P['edge_key'] = df_P['min_id'].astype(str) + '_' + df_P['max_id'].astype(str) + '_' + df_P['edge_label'].astype(str)
+        df_P_counts = df_P.groupby('edge_key').size().reset_index(name='count')
+        df_P = df_P.merge(df_P_counts, on='edge_key', how='left')
+        df_P['edge_type'] = df_P['count'].apply(lambda x: 'undirected' if x == 2 else 'directed')
+
+        df_pattern_edges = df_P[['src_id', 'dst_id', 'src_label', 'dst_label', 'edge_label', 'edge_type']].drop_duplicates()
+
+        df_sample = self._sample.df.copy()
+        df_sample['min_id'] = df_sample[['src_id', 'dst_id']].min(axis=1)
+        df_sample['max_id'] = df_sample[['src_id', 'dst_id']].max(axis=1)
+        df_sample['edge_key'] = df_sample['min_id'].astype(str) + '_' + df_sample['max_id'].astype(str) + '_' + df_sample['edge_label'].astype(str)
+        df_sample_counts = df_sample.groupby('edge_key').size().reset_index(name='count')
+        df_sample = df_sample.merge(df_sample_counts, on='edge_key', how='left')
+        df_sample['edge_type'] = df_sample['count'].apply(lambda x: 'undirected' if x == 2 else 'directed')
+
+        for idx, edge_p in df_pattern_edges.iterrows():
+            src_id_p = edge_p['src_id']
+            dst_id_p = edge_p['dst_id']
+            src_label_p = edge_p['src_label']
+            dst_label_p = edge_p['dst_label']
+            edge_label_p = edge_p['edge_label']
+            edge_type_p = edge_p['edge_type']
+
+            ui = dict_pv[src_id_p]
+            uj = dict_pv[dst_id_p]
+
+            if edge_type_p == 'directed':
+                df_match = df_sample[
+                    (df_sample['edge_type'] == 'directed') &
+                    (df_sample['edge_label'] == edge_label_p) &
+                    (df_sample['src_label'] == src_label_p) &
+                    (df_sample['dst_label'] == dst_label_p)
+                ][['src_id', 'dst_id']].copy()
+                df_match.columns = [ui, uj]
+            else:
+                df_match = df_sample[
+                    (df_sample['edge_type'] == 'undirected') &
+                    (df_sample['edge_label'] == edge_label_p) &
+                    (
+                        ((df_sample['src_label'] == src_label_p) & (df_sample['dst_label'] == dst_label_p)) |
+                        ((df_sample['src_label'] == dst_label_p) & (df_sample['dst_label'] == src_label_p))
+                    )
+                ][['src_id', 'dst_id']].copy()
+                df_match_rev = df_match.copy()
+                df_match_rev.columns = [uj, ui]
+                df_match.columns = [ui, uj]
+                df_match = pd.concat([df_match, df_match_rev], ignore_index=True)
+
+            df_match.drop_duplicates(inplace=True)
+            relation_size = len(df_match)
+
+            relations.append({'name': f'{ui},{uj}', 'size': relation_size, 'variables': set([ui, uj])})
+
+        relations.sort(key=lambda x: x['size'])
+
+        plan = []
+        used_variables = set()
+        remaining_relations = relations.copy()
+
+        while remaining_relations:
+            found = False
+            for idx, rel in enumerate(remaining_relations):
+                if not used_variables or used_variables & rel['variables']:
+                    plan.append([rel['name'], rel['size']])
+                    used_variables |= rel['variables']
+                    remaining_relations.pop(idx)
+                    found = True
+                    break
+            if not found:
+                rel = remaining_relations.pop(0)
+                plan.append([rel['name'], rel['size']])
+                used_variables |= rel['variables']
+
+        self._plan = plan
 
     def check_plan(self) -> bool:
         # do not modify this method
