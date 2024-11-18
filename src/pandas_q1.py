b6f837fcbcab6a5b245ab7779f4468504918e37b
VimalnathAchuthan <va33@illinois.edu>
2024-11-18 10:39:46 -0600

    completed pandas q1

--- src/pandas_q1.py
+++ src/pandas_q1.py
@@ -13,9 +13,27 @@ from src.report import get_file, report
 
 @report
 def pandas_q1(data_file: str, pattern_file: str) -> str:
-    # TODO: begin of your codes
-    return 'out/pandas_q1.csv'
-    # TODO: end of your codes
+    data_columns = ['src', 'dst', 'src_label', 'dst_label', 'edge_label']
+    pattern_columns = ['p_src', 'p_dst', 'p_src_label', 'p_dst_label', 'p_edge_label']
+
+    data = pd.read_csv(data_file, sep=" ", header=None, names=data_columns)
+    pattern = pd.read_csv(pattern_file, sep=" ", header=None, names=pattern_columns)
+
+    matched_edges = data.merge(
+        pattern,
+        left_on=['src_label', 'dst_label', 'edge_label'],
+        right_on=['p_src_label', 'p_dst_label', 'p_edge_label']
+    )
+
+    results = matched_edges[['src', 'dst', 'p_src', 'p_dst']]
+    results = results.rename(columns={'src': 'u1', 'dst': 'u2', 'p_src': 'p1', 'p_dst': 'p2'})
+
+    results = results.sort_values(by=['p1', 'p2', 'u1', 'u2'])
+
+    output_file = 'out/pandas_q1.csv'
+    results.to_csv(output_file, index=False, header=False)
+
+    return output_file
 
 
 def test(data, pattern, expect) -> int:
